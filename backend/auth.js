630e16d65e9d90f8ce9c9d14133b5347da129f1e
Tanusha Choudhary <144540796+tanushachoudhary@users.noreply.github.com>
2025-05-10 14:51:40 +0530

    Update auth.js

--- backend/auth.js
+++ backend/auth.js
@@ -7,7 +7,7 @@ passport.deserializeUser((obj, done) => done(null, obj));
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback",
+   callbackURL: "https://vendor-backend-2x0h.onrender.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
